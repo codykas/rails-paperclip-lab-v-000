68f95444e31768a113bd91c27a653dd4e18b8462
Cody Kasselman <ckasselman@fordham.edu>
2017-11-05 22:29:54 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-  has_attached_file :album_cover, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ":style/default.gif"
+  has_attached_file :album_cover, styles: { thumb: "100x100>" }, default_url: ":style/default.gif"
   validates_attachment_content_type :album_cover, content_type: /\Aimage\/.*\z/
 
   def artist_name
